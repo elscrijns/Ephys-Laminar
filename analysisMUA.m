d629e884155962083910693b652f325bb9f0ee88
elscrijns <els.crijns@ppw.kuleuven.be>
2017-03-27 15:06:08 +0200

    Update analysisMUA.m

--- analysisMUA.m
+++ analysisMUA.m
@@ -65,7 +65,7 @@ plotPSTHsPerCondition%_exp33
 clear before after lim
 %% Save the MU PSTH and datafile
 
-outFile = ['03-24_TC2_Ch21-27_MUA'];
+outFile = ['MM-DD_session_Ch##-##_MUA'];
     ha = axes('Position',[0 0 1 1],'Xlim',[0 1],'Ylim',[0 1],'Box','off','Visible','off','Units','normalized', 'clipping' , 'off');
     text(0.5,0.98, outFile,'HorizontalAlignment' ,'center','VerticalAlignment', 'top', 'FontSize', 14, 'Interpreter','none')
 
@@ -73,7 +73,7 @@ outFile = ['03-24_TC2_Ch21-27_MUA'];
     saveas(gca , figName, 'jpeg');
     
  fileName = [currentSession '\' outFile '.mat']; save(fileName, 'trial' );
-close all
+ close all
 % clearvars -except currentSession trial 
 
 %% Calculate PSTH per cluster
@@ -81,56 +81,26 @@ close all
 lim = [-3 20];
 for i = 1:nClusters
        
-selectedClusters = clusterIdentifiers(i);
-    if ismember(selectedClusters, [1])
-       continue; 
-    end
+    selectedClusters = clusterIdentifiers(i);
+        if ismember(selectedClusters, [1])
+           continue; 
+        end
 
-[selectedSpikeTimestampsInUsec, selectedClusters]   = extractMUdata(clusters, timings, timeStamps, selectedClusters);
-    before = 300;
-    after = 600;
-[trial] = linkSpikesAndBehaviour(trial, selectedSpikeTimestampsInUsec, before, after);
+    [selectedSpikeTimestampsInUsec, selectedClusters]   = extractMUdata(clusters, timings, timeStamps, selectedClusters);
+        before = 300;
+        after = 600;
+    [trial] = linkSpikesAndBehaviour(trial, selectedSpikeTimestampsInUsec, before, after);
 
-plotPSTHsPerCondition%_exp33
+    plotPSTHsPerCondition%_exp33
 
-spikes = size(selectedSpikeTimestampsInUsec,2);
-outFile = ['Cluster # ' num2str(selectedClusters') '_#' num2str(spikes)];
-    outFile = strrep(outFile, ' ', '_');
-    outFile = strrep(outFile, '__', '_');
-    outFile = strrep(outFile, '__', '_');
+    spikes = size(selectedSpikeTimestampsInUsec,2);
+    outFile = ['Cluster # ' num2str(selectedClusters') '_#' num2str(spikes)];
+        outFile = strrep(outFile, ' ', '_');
+        outFile = strrep(outFile, '__', '_');
+        outFile = strrep(outFile, '__', '_');
 
-ha = axes('Position',[0 0 1 1],'Xlim',[0 1],'Ylim',[0 1],'Box','off','Visible','off','Units','normalized', 'clipping' , 'off');
-text(0.5,0.98, outFile,'HorizontalAlignment' ,'center','VerticalAlignment', 'top', 'FontSize', 14, 'Interpreter','none')
-
-%figName = [currentSession '\PSTHs\' outFile '.jpeg'] ;
-%saveas(gca , figName, 'jpeg');
+    ha = axes('Position',[0 0 1 1],'Xlim',[0 1],'Ylim',[0 1],'Box','off','Visible','off','Units','normalized', 'clipping' , 'off');
+    text(0.5,0.98, outFile,'HorizontalAlignment' ,'center','VerticalAlignment', 'top', 'FontSize', 14, 'Interpreter','none')
 
 end
 
-%% Create raster plot per condition
-% spikes = {trial.spikes}.';
-% start = {trial.start}.';
-% condition = {trial.condition}.';
-% C = cellfun(@minus,spikes,start,'Un',0);
-% condition = [trial(1).condition].';
-% 
-% for i = 1:6
-%     conditionIndices = conditions == i;
-%     subplot(2,3,i);
-%     plotSpikeRaster(C(conditionIndices),'PlotType','vertline');
-%     xlim([0 900000])
-% end
-% %plotSpikeRaster(C,'PlotType','scatter');
-%% Save data
-% x       = strfind(currentSession, '- ') + 2;
-% session = currentSession(x:end);
-% outFile = [session '_Clu_' num2str(selectedClusters')];
-% outFile = strrep(outFile, ' ', '_');
-% outFile = strrep(outFile, '__', '_');
-% saveas(gca ,[currentSession '\' outFile '.tif'], 'tif');
-% csvwrite([currentSession '\trialFR_' outFile '.csv'] ,trialFR);
-% csvwrite([currentSession '\medianFR_' outFile '.csv'] ,medianFR);
-% csvwrite([currentSession '\PSTH_' outFile '.csv'] , psths);
-
-%close all
-%clear y trialFR Peaks medianFR selected* after before ans x outFile
